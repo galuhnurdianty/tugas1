1f99bd1b77f7a792b3d772f5d205e6f237660ddc
galuhnurdianty <32261792+galuhnurdianty@users.noreply.github.com>
2018-03-26 21:42:49 +0800

    Membuat Atribut dan Behavior

--- src/tugas1/Minum.java
+++ src/tugas1/Minum.java
@@ -10,5 +10,18 @@ package tugas1;
  * @author my computer
  */
 public class Minum {
+    String botol;
+    String vAir;
+    String rasa;
+    String warna;
+    String harga; 
     
+    void cetakInfo(){
+        System.out.println("Botol      :"+botol);
+        System.out.println("Volume Air :"+vAir);
+        System.out.println("Rasa       :"+rasa);
+        System.out.println("Warna      :"+warna);
+        System.out.println("Harga      :"+harga);
+        System.out.println("\"****************\"");
+    }
 }
