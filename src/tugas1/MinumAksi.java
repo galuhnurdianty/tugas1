7c26ffd6ee6fc0b27b915f0cd53f3bf7ebc17e83
galuhnurdianty <32261792+galuhnurdianty@users.noreply.github.com>
2018-03-26 21:46:20 +0800

    Membuat Class Aksi dan Method Main

---
+++ src/tugas1/MinumAksi.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas1;
+
+/**
+ *
+ * @author my computer
+ */
+public class MinumAksi {
+    public static void main(String[] args) {
+        
+    }
+}
