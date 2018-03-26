1848c7d8fcf0a8338cc032d33dbc81adf6080a74
galuhnurdianty <32261792+galuhnurdianty@users.noreply.github.com>
2018-03-26 21:51:30 +0800

    Mengisi Nilai Atribut

--- src/tugas1/MinumAksi.java
+++ src/tugas1/MinumAksi.java
@@ -14,5 +14,23 @@ public class MinumAksi {
         Minum kemasan_besar = new Minum();
         Minum kemasan_sedang = new Minum();
         Minum kemasan_kecil = new Minum();
+        
+        kemasan_besar.botol="Galon";
+        kemasan_besar.vAir="19L";
+        kemasan_besar.rasa="Tawar";
+        kemasan_besar.warna="Bening";
+        kemasan_besar.harga="17000";
+        
+        kemasan_sedang.botol="Fanta";
+        kemasan_sedang.vAir="5L";
+        kemasan_sedang.rasa="Manis";
+        kemasan_sedang.warna="Merah";
+        kemasan_sedang.harga="15000";
+        
+        kemasan_kecil.botol="Susu Beruang";
+        kemasan_kecil.vAir="250ml";
+        kemasan_kecil.rasa="Manis";
+        kemasan_kecil.warna="Putih";
+        kemasan_kecil.harga="8000";
     }
 }
